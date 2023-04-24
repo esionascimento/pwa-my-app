db4adf3855133404a3e17f404404431ddbe663e3
esionascimento <esionascimento@proton.me>
2023-04-24 01:52:54 -0400

    feat: not-found pages

---
+++ src/pages/not-found.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+
+const NotFound = () => {
+  return (
+    <Box>
+      <Box>Página não encontrada</Box>
+      <Box>...</Box>
+    </Box>
+  );
+};
+
+export default NotFound;
